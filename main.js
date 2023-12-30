c3c73295e44080f9e647533c7288300220035b46
saifur550 <saifurrahman@SAIFURs-MacBook-Pro.local>
2023-12-30 07:40:38 -0800

    Initial commit: file setup

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// ! for loop 
+
+let printItem =  'This line will print multiple time';
+
+for (let p = 1; p<=5; p++){
+    // 5 time will execute this line
+   // console.log(printItem);
+
+}
+
+ //console.log('loop in end here');
+
+
+ //! calculate sum of 1 to .....n  
+
+ let sum = 0;
+
+ for (let num = 0; num<=10; num++ ){
+       sum = sum + num;
+       //console.log('inside result ',sum);
+ }
+
+ console.log('outside result ', sum);
