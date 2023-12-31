8a96897c5080fff91a78536614117270ee4cf031
saifur550 <srstap550@gmail.com>
2023-12-31 14:05:04 -0800

    button function add

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
+// set initial count 
+let count = 0
 
-// ! for loop 
+//select button & value 
 
-let printItem =  'This line will print multiple time';
-
-for (let p = 1; p<=5; p++){
-    // 5 time will execute this line
-   // console.log(printItem);
-
-}
-
- //console.log('loop in end here');
+let value = document.querySelector('#value')
+let buttons = document.querySelectorAll('.btn')
 
 
- //! calculate sum of 1 to .....n  
+//console.log(btns);
 
- let sum = 0;
 
- for (let num = 0; num<=10; num++ ){
-       sum = sum + num;
-       //console.log('inside result ',sum);
- }
 
- console.log('outside result ', sum);
+buttons.forEach(element => {
+    element.addEventListener('click' , function(e){
+        let styles = e.currentTarget.classList; 
+        if(styles.contains('decrease')){
+            count--
+        }
+        value.textContent = count
+    })
+});
